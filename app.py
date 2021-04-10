b6cc65f177c8e22dd8c59f444166be3492f2749b
ThomasHTran <timtom9801gmail.com>
2021-04-10 15:13:09 -0700

    linking pages together

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('home.html')
+
 @app.route('/diary')
-def hello_world():
+def diary():
     return render_template('diary.html')
 
 if __name__ == '__main__':
