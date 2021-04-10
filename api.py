1c407ef45021d2857a877b3bab65de99dc528170
unknown <ayushnabar@outlook.com>
2021-04-10 02:38:38 -0700

    api stuff

---
+++ api.py
@@ -0,0 +1,16 @@
+import requests
+
+API_KEY = '2f82479a70b61e8898c5a891d16804bc'
+APP_ID = '8520a617'
+
+response = requests.get('https://api.edamam.com/api/nutrition-data?app_id=' + APP_ID + '&app_key=' + API_KEY + '&ingr=1%20large%20apple')
+json = response.json()
+
+def get_fat():
+    return json['totalNutrients']['FAT']['quantity']
+def get_protein():
+    return json['totalNutrients']['PROCNT']['quantity']
+def get_carbs():
+    return json['totalNutrients']['CHOCDF']['quantity']
+
+
