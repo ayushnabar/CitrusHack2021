ca840b450bf0bb5a82eb502bb078867e87bfd7ba
awang923 <jerrywang@annies-macBook-Pro.local>
2021-04-10 04:22:04 -0700

    firebase

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+users = [{'uid': 1, 'name': 'Bob'}]
+
+@app.route('/api/userinfo')
+def userinfo():
+    return {'data': users},200
+
 @app.route('/')
 def hello_world():
     return render_template('template.html')
